9da616afe29c1279152ac9b01d5ae745fda777ea
Christian Molkenthin <christian.molkenthin@uni-potsdam.de>
2020-08-25 16:59:42 +0200

    some test changes to test conections

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='GPetas',
+    version='0.0.0',
+    packages=['GPetas'],
+    url='',
+    license='',
+    author='chrism',
+    author_email='',
+    description=''
+)
